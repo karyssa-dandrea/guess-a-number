880f70405c074bb95311f8f4fe60735626e87441
karyssa-dandrea <karyssadandrea@gmail.com>
2021-10-04 16:51:48 -0700

    enabled buttons to show guesses and for guess button to go away once the guesses are up

--- app.js
+++ app.js
@@ -1,29 +1,36 @@
 // import functions and grab DOM elements
+console.log('i am here');
 const guessButton = document.getElementById('guess-button');
 const userGuess = document.getElementById('guess');
+const userMessage = document.getElementById('user-message');
+const chances = document.getElementById('chances');
 console.log('guess-button');
 
 
 
 // initialize global state
 let randomNum = Math.ceil(Math.random() * 20);
-let numGuesses = 0;
+let numGuesses = 4;
 // set event listeners 
-guessButton.addEventListener('click', ()=>{
-numGuesses = numGuesses + 1;
-const userGuessValue = Number(userGuess.value);
-console.log(userGuessValue);
-console.log(randomNum);
-if (userGuessValue === randomNum) {
-  console.log("You win!");
-} else if (numGuesses === 4){
-console.log("Out of guesses!");
 
+guessButton.addEventListener('click', ()=>{
+numGuesses--;
+
+console.log(numGuesses);
+const userGuessValue = Number(userGuess.value);
+chances.textContent = numGuesses;
+
+if (userGuessValue === randomNum) {
+  userMessage.textContent = 'You Win!'
+} else if (numGuesses === 0){
+  userMessage.textContent = 'Out Of Guesses!'
+guessButton.disabled = true;
 } else if (userGuessValue > randomNum) {
-  console.log("Guess too high");
+  userMessage.textContent = 'Guess too high!'
 } else if (userGuessValue < randomNum) {
-  console.log("Guess too low");
-}
+  userMessage.textContent = 'Guess too low!'
+
+} 
 
 })
 
